e2781b3c60e6235d337ffd73189653a22ab061f4
Nicolas Guillemot <nlguillemot@gmail.com>
2012-07-17 12:43:01 -0700

    fixed privacy issues of GameApp. Tinkering with AnimatedSprite. Improved HashedString algorithm.

--- source/include/viking/HashedString.hpp
+++ source/include/viking/HashedString.hpp
@@ -3,9 +3,11 @@
 
 #include <irrlicht/irrTypes.h>
 
-// uncomment the next line to use constexpr hashing algorithm
+// if HASHEDSTRING_USE_CONSTEXPR is defined, a recursive constexpr hashing algorithm will be used. 
+// otherwise, an iterative runtime hashing algorithm will be used.
 #define HASHEDSTRING_USE_CONSTEXPR
 
+// do not touch this block of preprocessor.
 #ifdef HASHEDSTRING_USE_CONSTEXPR
 	#define HASHEDSTRING_CONSTEXPR_IMPL constexpr
 #else
@@ -21,7 +23,12 @@ namespace vik
 // strings are hashed at compile time whenever possible using constexpr, if the HASHEDSTRING_USE_CONSTEXPR flag is defined.
 class HashedString
 {
+private:
+	typedef irr::u32 HashType;
 public:
+	friend HASHEDSTRING_CONSTEXPR_IMPL HashType _hashString(const char* str);
+	friend HASHEDSTRING_CONSTEXPR_IMPL HashType _hashStringRecursive(HashType hash, const char* str);
+
 	// Hashes the passed in string and stores the hash
 	// in debug builds, the original string pointer will also be stored to be accessed with getDebugString()
 	inline explicit HASHEDSTRING_CONSTEXPR_IMPL HashedString(const char* str);
@@ -32,17 +39,12 @@ public:
 	inline const char* getReverseHash() const;
 
 	// compares hash by equality
-	inline bool operator==(const HashedString& other) const;
+	inline constexpr bool operator==(const HashedString& other) const;
+	inline constexpr bool operator!=(const HashedString& other) const;
 	// compares hash in terms of order to allow efficient use with std::less<T> as a Compare function in std::maps
-	inline bool operator<(const HashedString& other) const;
+	inline constexpr bool operator<(const HashedString& other) const;
 	// add more comparison operators as they are needed. Always compare with the hash, not the reverse hash.
 private:
-	typedef irr::u32 HashType;
-
-	// performs a compile time recursive string hash using the djb2 algorithm explained here: http://www.cse.yorku.ca/~oz/hash.html
-	static inline HASHEDSTRING_CONSTEXPR_IMPL HashType hashString(const char* str);
-	static inline HASHEDSTRING_CONSTEXPR_IMPL HashType hashStringRecursive(HashType hash, const char* str);
-
 	// reverse hash only exists in debug build
 #ifdef _DEBUG
 	const char* debugReverseHash;
@@ -50,26 +52,20 @@ private:
 	HashType hash;
 };
 
-HASHEDSTRING_CONSTEXPR_IMPL HashedString::HashedString(const char* str):
-#ifdef _DEBUG
-debugReverseHash(str),
-#endif
-hash(hashString(str))
-{
-}
-
-HASHEDSTRING_CONSTEXPR_IMPL HashedString::HashType HashedString::hashStringRecursive(HashType hash, const char* str)
+// performs a compile time recursive string hash using the djb2 algorithm explained here: http://www.cse.yorku.ca/~oz/hash.html
+// To hash a string, do not call these functions. Instead, construct a HashedString with the string passed as an argument to the constructor.
+HASHEDSTRING_CONSTEXPR_IMPL HashedString::HashType _hashStringRecursive(HashedString::HashType hash, const char* str)
 {
 	return ( !*str ? hash :
-		hashStringRecursive(((hash << 5) + hash) + *str, str + 1)); 
+		_hashStringRecursive(((hash << 5) + hash) + *str, str + 1)); 
 }
 
 // performs a compile time string hash
-HASHEDSTRING_CONSTEXPR_IMPL HashedString::HashType HashedString::hashString(const char* str)
+HASHEDSTRING_CONSTEXPR_IMPL HashedString::HashType _hashString(const char* str)
 {
 #ifdef HASHEDSTRING_USE_CONSTEXPR
 	return ( !str ? 0 :
-		hashStringRecursive(5381, str));
+		_hashStringRecursive(5381, str));
 #else
 	HashType hash = 5381;
 	int x;
@@ -83,6 +79,14 @@ HASHEDSTRING_CONSTEXPR_IMPL HashedString::HashType HashedString::hashString(cons
 #endif
 }
 
+HASHEDSTRING_CONSTEXPR_IMPL HashedString::HashedString(const char* str):
+#ifdef _DEBUG
+debugReverseHash(str),
+#endif
+hash(_hashString(str))
+{
+}
+
 const char* HashedString::getReverseHash() const
 {
 #ifdef _DEBUG
@@ -92,12 +96,17 @@ const char* HashedString::getReverseHash() const
 #endif
 }
 
-bool HashedString::operator==(const HashedString& other) const
+bool constexpr HashedString::operator==(const HashedString& other) const
 {
 	return hash == other.hash;
 }
 
-bool HashedString::operator<(const HashedString& other) const
+bool constexpr HashedString::operator!=(const HashedString& other) const
+{
+	return hash != other.hash;
+}
+
+bool constexpr HashedString::operator<(const HashedString& other) const
 {
 	return hash < other.hash;
 }
